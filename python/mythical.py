6a270284c0fce8f20afbbdb9a489ba0b165f8518
shoghills <rifadanindra@gmail.com>
2025-10-03 21:12:07 +0700

    Delete python directory

--- python/mythical.py
+++
@@ -1,4 +0,0 @@
-class MakhlukMitos:
-    def __init__(self, kekuatan, kekuatanSpesial):
-        self.kekuatan = kekuatan
-        self.kekuatanSpesial = kekuatanSpesial
--- python/reptile.py
+++
@@ -1,9 +0,0 @@
-class Reptile:
-    def __init__(self, nama, panjang, habitat, diet):
-        self.nama = nama
-        self.panjang = panjang
-        self.habitat = habitat
-        self.diet = diet
-
-    def info(self):
-        return f"{self.nama} | Panjang: {self.panjang}m | Habitat: {self.habitat} | Diet: {self.diet}"
